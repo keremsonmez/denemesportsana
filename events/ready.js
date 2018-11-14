ad965b026f9b4fd1ebe98c47cbf71d30d7c21fae
keremsonmez <44930552+keremsonmez@users.noreply.github.com>
2018-11-14 23:09:41 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => {
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
   client.user.setStatus("online");
-  client.user.setGame(`${prefix}yardım + ${client.guilds.size} sunucu + ${client.users.size} kullanıcı`);
+  client.user.setPresence({game:{name:`Yardım Komutları İçin ${prefix}yardım | ${client.guilds.size} Sunucuda hizmet vermekte | ${client.users.size} Kullanıcı | Müzik komutları için ${prefix}muzikyardım | Versiyon 0.2`, type: 0}});
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
 };
